3dbe001812e1ffb566604d46e3155163dd74c403
Juan Pablo Azambuyo <juanpabloazambuyo@gmail.com>
2022-11-22 17:22:12 -0300

    h

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const { Router } = require('express');
 const axios = require('axios');
 require('dotenv').config();
 const { API_KEY1 , API_KEY2 , API_KEY3 , API_KEY4 , API_KEY5 } = process.env;
-const API_KEY = API_KEY1;
+const API_KEY = API_KEY2;
 const NUMBER = 3;
 const { Recipes , Diets , Op } = require('../db.js');
 
