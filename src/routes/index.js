f6a5dcccf4eaba8d8b0e134b416c4036ac657d20
PabloAza89 <juanpabloazambuyo@gmail.com>
2022-11-22 03:23:06 -0300

    h

--- src/routes/index.js
+++ src/routes/index.js
@@ -142,27 +142,6 @@ router.post('/recipes', async (req, res) => {
     }
 });
 
-router.post('/diets', async (req, res) => {
-    try {
-        res.json(await Diets.bulkCreate([      
-            { title: "All Diets", },
-            { title: "Gluten Free", },
-            { title: "Ketogenic" },
-            { title: "Vegan" },
-            { title: "Lacto Ovo Vegetarian" },
-            { title: "Pescatarian" },
-            { title: "Paleolithic" },
-            { title: "Primal" },
-            { title: "Fodmap Friendly" },
-            { title: "Whole 30" },
-            { title: "Dairy Free" }
-          ]))
-    }
-    catch(e) {
-        res.status(400).send('Las dietas ya estan precargadas')
-    } 
-  });
-
 router.get('/diets', async (req, res) => {
     try {
         const diets = await Diets.findAll()
