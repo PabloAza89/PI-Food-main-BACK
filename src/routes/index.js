b6454e1677b2a58051076caf47521624b061d6f6
PabloAza89 <juanpabloazambuyo@gmail.com>
2022-11-22 20:04:39 -0300

    h

--- src/routes/index.js
+++ src/routes/index.js
@@ -16,9 +16,8 @@ const router = Router();
 // Ejemplo: router.use('/auth', authRouter);
 
 let allApiResults = async () => {
-    const apiRawData = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&number=${NUMBER}&addRecipeInformation=true`);
-    const asd = await apiRawData
-    return await asd.data.results.map(e => {
+    const apiRawData = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=f02bdac78602401eb4a22dc35033d573&number=3&addRecipeInformation=true`);
+    return apiRawData.data.results.map(e => {
         return {
             id: e.id,
             title: e.title,
