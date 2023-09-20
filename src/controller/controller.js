ac550de59ad5d8c49ae5b1e48720669a8df46031
Juan Pablo Azambuyo <juanpabloazambuyo@gmail.com>
2023-09-20 20:14:45 -0300

    developed checkUser function

---
+++ src/controller/controller.js
@@ -0,0 +1,117 @@
+const axios = require('axios');
+const NUMBER = 1;
+
+const allApiResults = async (props) => {
+  const apiRawData = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${props.key}&number=${NUMBER}&addRecipeInformation=true`)
+    .then(res => { return res })
+    .catch(err => { return err })
+
+  //console.log("Response bad", apiRawData.response && apiRawData.response.status) // bad = 402
+
+  if (apiRawData.response && apiRawData.response.status === 402) return { status: 402, message: "Expired key", ok: false, try: props.try }
+  else return apiRawData.data && apiRawData.data.results.map(e => {
+    return {
+      id: e.id,
+      title: e.title,
+      summary: e.summary,
+      healthScore: e.healthScore,
+      analyzedInstructions:
+        e.analyzedInstructions[0] ?
+        e.analyzedInstructions[0].steps.map(e=> e.step) :
+        [],
+      image: e.image,
+      diets:
+        e.diets.map(function(e) {
+          if ((e.indexOf(e) !== e.length - 1)) return e.split(" ").map(e => e[0].toUpperCase() + e.slice(1)).join(" ")
+          else return e.split(" ").map(e => e[0].toUpperCase() + e.slice(1)).join(" ")
+        }),
+      dishTypes: e.dishTypes,
+      userRecipe: false
+    }
+  })
+}
+
+const checkUser = async (props) => {
+  const minutes = 2880 // 1 hour = 60 // 12 hours = 720 // 1 day = 1440 // 7 days = 10080
+    let options = {
+      path: "/",
+      expires: new Date(Date.now() + (60 * 1000 * minutes)),
+      //domain: 'localhost:3001',
+      httpOnly: true
+    }
+
+  if (props.onlyCheck) {
+    if (props.fd_ck_tkn !== undefined) {
+      let response = await axios.get(`https://www.googleapis.com/oauth2/v3/userinfo?access_token=${props.fd_ck_tkn}`)
+        .then(res => { return res })
+        .catch(err => { return err })
+      console.log("PASO POR ACA 1")
+      //console.log("response.data", response.data)
+      //console.log("response.dataaa", response)
+      if (response.data && response.data.email !== undefined) {
+        console.log("ESTE A")
+        return {
+          userValid: true,
+          fd_tkn: false,
+          fd_ck_tkn: props.fd_ck_tkn,
+          email: response.data.email,
+          options: options
+        }
+      } else {
+        console.log("ESTE B")
+        return { userValid: false }
+      }
+
+    } else if (props.fd_tkn !== undefined) {
+      let response = await axios.get(`https://www.googleapis.com/oauth2/v3/userinfo?access_token=${props.fd_tkn}`)
+      console.log("PASO POR ACA 2")
+      if (response.data && response.data.email !== undefined) {
+        return {
+          userValid: true,
+          fd_tkn: props.fd_tkn,
+          fd_ck_tkn: false,
+          email: response.data.email,
+          options: options
+        }
+      } else {
+        return { userValid: false }
+      }
+    }
+    
+  } else {
+    if (props.fd_ck_tkn && !props.fd_tkn) {
+      let response = await axios.get(`https://www.googleapis.com/oauth2/v3/userinfo?access_token=${props.fd_ck_tkn}`)
+      //.catch(err => {throw err})
+  
+      if (response.data.email !== undefined) {
+        //console.log("response.data", response.data)
+        props.res.cookie('fd_ck_tkn', props.fd_ck_tkn, options)
+        return props.res.status(200).json({ email: response.data.email, fd_tkn: props.fd_ck_tkn, status: 200 })
+      }
+    }
+  
+    if (!props.fd_ck_tkn && props.fd_tkn) { // food_cookie_token // food_token
+      props.res.cookie('fd_ck_tkn', props.fd_tkn, options)
+      return props.res.status(200).json({ status: 200 })
+    }
+
+  }
+
+  // if (data.fd_ck_tkn && !data.fd_tkn) {
+  //   let response = await axios.get(`https://www.googleapis.com/oauth2/v3/userinfo?access_token=${data.fd_ck_tkn}`)
+  //   //.catch(err => {throw err})
+
+  //   if (response.data.email !== undefined) {
+  //     data.res.cookie('fd_ck_tkn', data.fd_ck_tkn, options)
+  //     return data.res.status(200).json({ email: response.data.email, fd_tkn: data.fd_ck_tkn, status: 200 })
+  //   }
+  // }
+
+  // if (!data.fd_ck_tkn && data.fd_tkn) { // food_cookie_token // food_token
+  //   data.res.cookie('fd_ck_tkn', data.fd_tkn, options)
+  //   return data.res.status(200).json({ status: 200 })
+  // }
+
+}
+
+module.exports = { allApiResults, checkUser }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,9 @@
 const cloudinary = require('cloudinary').v2
-var cookieParser = require('cookie-parser')
 const { Router } = require('express');
+const { allApiResults, checkUser } = require('../controller/controller');
 const axios = require('axios');
 require('dotenv').config();
 const { CLD_NAME, CLD_KEY, CLD_SECRET, API_KEY1 , API_KEY2 , API_KEY3 , API_KEY4 , API_KEY5 } = process.env;
-const NUMBER = 1;
 const { Recipes , Diets , Dishes, Op } = require('../db.js');
 
 const router = Router();
@@ -15,50 +14,16 @@ cloudinary.config({
   api_secret: CLD_SECRET
 });
 
-let allApiResults = async (data) => {
-
-  const apiRawData = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${data.key}&number=${NUMBER}&addRecipeInformation=true`)
-    .then(res => { return res })
-    .catch(err => { return err })
-
-  //console.log("Response bad", apiRawData.response && apiRawData.response.status) // bad = 402
-
-  if (apiRawData.response && apiRawData.response.status === 402) return { status: 402, message: "Expired key", ok: false, try: data.try }
-
-  else return apiRawData.data && apiRawData.data.results.map(e => {
-    return {
-      id: e.id,
-      title: e.title,
-      summary: e.summary,
-      healthScore: e.healthScore,
-      analyzedInstructions:
-        e.analyzedInstructions[0] ?
-        e.analyzedInstructions[0].steps.map(e=> e.step) :
-        [],
-      image: e.image,
-      diets:
-        e.diets.map(function(e) {
-          if ((e.indexOf(e) !== e.length - 1)) return e.split(" ").map(e => e[0].toUpperCase() + e.slice(1)).join(" ")
-          else return e.split(" ").map(e => e[0].toUpperCase() + e.slice(1)).join(" ")
-        }),
-      dishTypes: e.dishTypes,
-      userRecipe: false
-    }
-  })
-}
-
 router.get('/recipes', async (req, res) => {
   const { title } = req.query;
 
-  function ifTitleExists () {
-    return title ? { title: {[Op.like]: `%${req.query.title.toLowerCase()}%`}} : {}
-  }
+  // function ifTitleExists () {
+  //   return title ? { title: {[Op.like]: `%${req.query.title.toLowerCase()}%`}} : {}
+  // }
 
    // try {
         const searchDBRecipes = await Recipes.findAll({
-          where:
-          ifTitleExists()
-          ,
+          where: title ? { title: {[Op.like]: `%${req.query.title.toLowerCase()}%`}} : {},
           include: [
             {
               model: Diets,
@@ -87,7 +52,7 @@ router.get('/recipes', async (req, res) => {
             analyzedInstructions: e.analyzedInstructions,
             userRecipe: e.userRecipe,
             email: e.email,
-            dishes: e.Dishes.map(e => e.title),
+            dishTypes: e.Dishes.map(e => e.title.toLowerCase()),
             diets: e.Diets.map(e => e.title)
           }
         })
@@ -123,70 +88,10 @@ router.get('/recipes', async (req, res) => {
     // }
 });
 
-router.get('/test', async (req, res) => {
+router.delete('/detele', async (req, res) => {
   const { title } = req.query;
 
-  function ifTitleExists () {
-    return title ? { title: {[Op.like]: `%${req.query.title.toLowerCase()}%`}} : {}
-  }
 
-   // try {
-        const searchDBRecipes = await Recipes.findAll({
-          where:
-          ifTitleExists()
-          ,
-          include: [
-            {
-              model: Diets,
-              attributes: ['title'],
-              through: {
-                attributes: []
-              }
-            },
-            {
-              model: Dishes,
-              attributes: ['title'],
-              through: {
-                attributes: []
-              }
-            }
-          ]
-        })
-
-        //console.log("AAA", searchDBRecipes[0].dataValues)
-        //console.log("AAA", searchDBRecipes.map(e => e.Diets).map(e => e.map(e => e.title)).flat())
-        //console.log("AAA", searchDBRecipes.map(e => e.Diets).map(e => e.map(e => e.title)))
-        console.log("AAA", searchDBRecipes)
-        //console.log("AAA", searchDBRecipes.map(e => e.Diets).map(e => e.map(e => e.title).flat()))
-
-
-        let qq = searchDBRecipes.map((e) => {
-          return {
-            id: e.id,
-            title: e.title,
-            image: e.image,
-            healthScore: e.healthScore,
-            summary: e.summary,
-            analyzedInstructions: e.analyzedInstructions,
-            userRecipe: e.userRecipe,
-            email: e.email,
-            dishes: e.Dishes.map(e => e.title),
-            diets: e.Diets.map(e => e.title)
-          }
-        })
-
-
-        return res.status(200).json({ message: qq })
-        
-    // }
-    // catch (err) {
-      //console.log("api error", err)
-        
-        //if (err.response.data.code === 402) res.status(400).json({ status: 400, message: "Expired key", ok: false })
-        //else res.status(400).json({ status: 400, message: "Error", ok: false })
-        //res.status(400).json({ status: 400, message: "Error", ok: false })
-        
-    // }
 });
 
 router.get('/recipes/:id', async (req, res) => {
@@ -238,14 +143,81 @@ router.get('/recipes/:id', async (req, res) => {
     }
 });
 
+// router.post('/recipes', async (req, res) => {
+
+//   const {
+//     title, image, healthScore, summary,
+//     dishes, diets, analyzedInstructions, email
+//   } = req.body;
+
+//   console.log("image",image)
+
+//   try {
+//     if (
+//       title.replaceAll(" ","").replaceAll("\n", "") === "" ||
+//       healthScore === "" ||
+//       summary.replaceAll(" ","").replaceAll("\n", "") === "" ||
+//       dishes.length === 0 ||
+//       diets.length === 0 ||
+//       analyzedInstructions.map(e => e.replaceAll(" ","").replaceAll("\n","")).some(e => e === "") ||
+//       email.replaceAll(" ","").replaceAll("\n", "") === ""
+//     ) res.status(400).json({'status': 400})
+//     else {
+//         const createRecipe = await Recipes.create({
+//           title: title,
+//           healthScore: parseInt(healthScore),
+//           summary: summary,
+//           image: image,
+//           analyzedInstructions: analyzedInstructions,
+//           userRecipe: true,
+//           email: email
+//         });
+//       const relatedDiets = await Diets.findAll({
+//           where: { [Op.or]: [ { title: diets } ] }
+//       })
+//       const relatedDishes = await Dishes.findAll({
+//         where: { [Op.or]: [ { title: dishes } ] }
+//       })
+
+//       createRecipe.addDiets(relatedDiets)
+//       createRecipe.addDishes(relatedDishes)
+
+//       cloudinary.uploader.upload(image, { public_id: createRecipe.id, overwrite: true, invalidate: true })
+//       .then(async(res) => {
+//         //console.log("RES", res)
+//         const updateRecipe = await Recipes.findOne({
+//           where: { id: createRecipe.id }
+//         });
+//         updateRecipe.image = createRecipe.id;
+//         await updateRecipe.save();
+//       })
+//       .catch(async() => {
+//         //console.log(ere)
+//         const updateRecipe = await Recipes.findOne({
+//           where: { id: createRecipe.id }
+//         });
+//         updateRecipe.image = Math.ceil(Math.random() * 3);
+//         await updateRecipe.save();
+//       })
+
+//       res.status(200).json({ 'status': 200 })
+//     }
+//   } catch(e) {
+//     res.status(400).json({'status': 400, 'error': e})
+//   }
+// });
+
 router.post('/recipes', async (req, res) => {
-
   const {
-    title, image, healthScore, summary,
-    dishes, diets, analyzedInstructions, email
+    title, image, healthScore, summary, email,
+    dishes, diets, analyzedInstructions, fd_tkn
   } = req.body;
+  const { fd_ck_tkn } = req.cookies;
+  console.log("email", email, "fd_tkn", fd_tkn, "fd_ck_tkn", fd_ck_tkn)
 
-  console.log("image",image)
+  //console.log("image", image)
+
+  //let onlyCheck = true
 
   try {
     if (
@@ -254,49 +226,59 @@ router.post('/recipes', async (req, res) => {
       summary.replaceAll(" ","").replaceAll("\n", "") === "" ||
       dishes.length === 0 ||
       diets.length === 0 ||
-      analyzedInstructions.map(e => e.replaceAll(" ","").replaceAll("\n","")).some(e => e === "") ||
-      email.replaceAll(" ","").replaceAll("\n", "") === ""
-    ) res.status(400).json({'status': 400})
-    else {
-        const createRecipe = await Recipes.create({
-          title: title,
-          healthScore: parseInt(healthScore),
-          summary: summary,
-          image: image,
-          analyzedInstructions: analyzedInstructions,
-          userRecipe: true,
-          email: email
-        });
-      const relatedDiets = await Diets.findAll({
-          where: { [Op.or]: [ { title: diets } ] }
-      })
-      const relatedDishes = await Dishes.findAll({
-        where: { [Op.or]: [ { title: dishes } ] }
-      })
+      analyzedInstructions.map(e => e.replaceAll(" ","").replaceAll("\n","")).some(e => e === "")
+    ) return res.status(400).json({'status': 400})
+    else if (fd_ck_tkn !== undefined || fd_tkn !== undefined) {
+      let checkUserResponse = await checkUser({ onlyCheck: true, res: res, fd_tkn: fd_tkn, fd_ck_tkn: fd_ck_tkn })
+      console.log("checkUserResponse", checkUserResponse)
+      //if (checkUserResponse === `Valid use`)
+      if (checkUserResponse.userValid) {
+            const createRecipe = await Recipes.create({
+              title: title,
+              healthScore: parseInt(healthScore),
+              summary: summary,
+              image: image,
+              analyzedInstructions: analyzedInstructions,
+              userRecipe: true,
+              email: checkUserResponse.email
+            });
+          const relatedDiets = await Diets.findAll({
+              where: { [Op.or]: [ { title: diets } ] }
+          })
+          const relatedDishes = await Dishes.findAll({
+            where: { [Op.or]: [ { title: dishes } ] }
+          })
 
-      createRecipe.addDiets(relatedDiets)
-      createRecipe.addDishes(relatedDishes)
+          createRecipe.addDiets(relatedDiets)
+          createRecipe.addDishes(relatedDishes)
 
-      cloudinary.uploader.upload(image, { public_id: createRecipe.id, overwrite: true, invalidate: true })
-      .then(async(res) => {
-        //console.log("RES", res)
-        const updateRecipe = await Recipes.findOne({
-          where: { id: createRecipe.id }
-        });
-        updateRecipe.image = createRecipe.id;
-        await updateRecipe.save();
-      })
-      .catch(async() => {
-        //console.log(ere)
-        const updateRecipe = await Recipes.findOne({
-          where: { id: createRecipe.id }
-        });
-        updateRecipe.image = Math.ceil(Math.random() * 3);
-        await updateRecipe.save();
-      })
+          cloudinary.uploader.upload(image, { public_id: createRecipe.id, overwrite: true, invalidate: true })
+          .then(async(res) => {
+            //console.log("RES", res)
+            const updateRecipe = await Recipes.findOne({
+              where: { id: createRecipe.id }
+            });
+            updateRecipe.image = createRecipe.id;
+            await updateRecipe.save();
+          })
+          .catch(async() => {
+            //console.log(ere)
+            const updateRecipe = await Recipes.findOne({
+              where: { id: createRecipe.id }
+            });
+            updateRecipe.image = Math.ceil(Math.random() * 3);
+            await updateRecipe.save();
+          })
+          res.cookie('fd_ck_tkn', checkUserResponse.fd_tkn ? checkUserResponse.fd_tkn : checkUserResponse.fd_ck_tkn, checkUserResponse.options)
+          res.status(200).json({ 'status': 200 })
 
-      res.status(200).json({ 'status': 200 })
+      } else {
+        res.clearCookie("fd_ck_tkn")
+        return res.status(400).json({ status: 400, message: `Invalid Credentials`, ok: false })
+      }
+      //return console.log("qq", qq)
     }
+    
   } catch(e) {
     res.status(400).json({'status': 400, 'error': e})
   }
@@ -360,37 +342,99 @@ router.get('/dishes', async (req, res) => { // THIS ROUTE ALWAYS RETURN DIETS.
   }
 });
 
+// router.post('/user', async (req, res) => {
+//   const { email, token } = req.body;
+//   console.log("email", email, "token", token, "fd_tkn", req.cookies.fd_tkn)
+
+//   try {
+//     const minutes = 2880 // 1 hour = 60 // 12 hours = 720 // 1 day = 1440 // 7 days = 10080
+//       let options = {
+//         path: "/",
+//         expires: new Date(Date.now() + (60 * 1000 * minutes)),
+//         //domain: 'localhost:3001',
+//         httpOnly: true
+//       }
+
+//     if (req.cookies.fd_tkn && !token) {
+//       let response = await axios.get(`https://www.googleapis.com/oauth2/v3/userinfo?access_token=${req.cookies.fd_tkn}`)
+
+//       if (response.data.email !== undefined) {
+//         res.cookie('fd_tkn', req.cookies.fd_tkn, options)
+//         res.status(200).json({ email: response.data.email, token: req.cookies.fd_tkn, status: 200 })
+//       }
+//     }
+
+//     if (!req.cookies.fd_tkn && token) {
+//       res.cookie('fd_tkn', token, options)
+//       res.status(200).json({ status: 200 })
+//     }
+
+//   }
+//   catch(err) {
+//     if (err.response.data.error_description === 'Invalid Credentials') {
+//       res.clearCookie("fd_tkn")
+//       return res.status(400).json({ status: 400, message: err.response.data.error_description, ok: false })
+//     } else return res.status(400).json({ status: 400, message: "error", ok: false })
+//   }
+// });
+
+// router.post('/user', async (req, res) => {
+//   const { email, fd_tkn } = req.body;
+//   console.log("email", email, "fd_tkn", fd_tkn, "fd_ck_tkn", req.cookies.fd_ck_tkn)
+
+//   try {
+//     //allApiResults({ key: keysArray[j], try: j + 1 })
+//     //await checkUser({ fd_ck_tkn: req.cookies.fd_ck_tkn, fd_tkn: fd_tkn })
+
+//     const minutes = 2880 // 1 hour = 60 // 12 hours = 720 // 1 day = 1440 // 7 days = 10080
+//     let options = {
+//       path: "/",
+//       expires: new Date(Date.now() + (60 * 1000 * minutes)),
+//       //domain: 'localhost:3001',
+//       httpOnly: true
+//     }
+
+//   if (req.cookies.fd_ck_tkn && !fd_tkn) {
+//     let response = await axios.get(`https://www.googleapis.com/oauth2/v3/userinfo?access_token=${req.cookies.fd_ck_tkn}`)
+
+//     if (response.data.email !== undefined) {
+//       res.cookie('fd_ck_tkn', req.cookies.fd_ck_tkn, options)
+//       res.status(200).json({ email: response.data.email, fd_tkn: req.cookies.fd_ck_tkn, status: 200 })
+//     }
+//   }
+
+//   if (!req.cookies.fd_ck_tkn && fd_tkn) { // food_cookie_token // food_token
+//     res.cookie('fd_ck_tkn', fd_tkn, options)
+//     res.status(200).json({ status: 200 })
+//   }
+
+//   }
+//   catch(err) {
+//     if (err.response.data.error_description === 'Invalid Credentials') {
+//       res.clearCookie("fd_ck_tkn")
+//       return res.status(400).json({ status: 400, message: err.response.data.error_description, ok: false })
+//     } else return res.status(400).json({ status: 400, message: "error", ok: false })
+//   }
+// });
+
 router.post('/user', async (req, res) => {
-  const { email, token } = req.body;
-  console.log("email", email, "token", token, "fd_tkn", req.cookies.fd_tkn)
+  const { email, fd_tkn } = req.body;
+  const { fd_ck_tkn } = req.cookies;
+  console.log("email", email, "fd_tkn", fd_tkn, "fd_ck_tkn", fd_ck_tkn)
+
+  //let onlyCheck = false
 
   try {
-    const minutes = 2880 // 1 hour = 60 // 12 hours = 720 // 1 day = 1440 // 7 days = 10080
-      let options = {
-        path: "/",
-        expires: new Date(Date.now() + (60 * 1000 * minutes)),
-        //domain: 'localhost:3001',
-        httpOnly: true
-      }
-
-    if (req.cookies.fd_tkn && !token) {
-      let response = await axios.get(`https://www.googleapis.com/oauth2/v3/userinfo?access_token=${req.cookies.fd_tkn}`)
-
-      if (response.data.email !== undefined) {
-        res.cookie('fd_tkn', req.cookies.fd_tkn, options)
-        res.status(200).json({ email: response.data.email, token: req.cookies.fd_tkn, status: 200 })
-      }
-    }
-
-    if (!req.cookies.fd_tkn && token) {
-      res.cookie('fd_tkn', token, options)
-      res.status(200).json({ status: 200 })
-    }
-
+    
+    //let qq = await checkUser({ onlyCheck: onlyCheck, res: res, fd_ck_tkn: req.cookies.fd_ck_tkn, fd_tkn: fd_tkn })
+    return await checkUser({ res: res, fd_ck_tkn: fd_ck_tkn, fd_tkn: fd_tkn })
+    
+    //return onlyCheck ? console.log("qq", qq) : qq
+    
   }
   catch(err) {
     if (err.response.data.error_description === 'Invalid Credentials') {
-      res.clearCookie("fd_tkn")
+      res.clearCookie("fd_ck_tkn")
       return res.status(400).json({ status: 400, message: err.response.data.error_description, ok: false })
     } else return res.status(400).json({ status: 400, message: "error", ok: false })
   }
